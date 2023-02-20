3599570b63f73df18fea7209b2e20ef053959995
unknown <dwizulyanhamid@gmail.com>
2023-02-20 22:30:07 +0700

    first init

---
+++ index.js
@@ -0,0 +1,18 @@
+const buttonKaget = document.querySelector("[data-button-kaget]");
+const img = document.querySelector(".img");
+const auido = document.querySelector("[data-audio-kaget]");
+let counter = document.querySelector("[data-count]");
+counter.innerHTML = `Pakde Owi Sudah Kaget ${localStorage.clickcount} kali`;
+buttonKaget.addEventListener("click", function () {
+  img.src = "./assets/images/pakde_kaget.png";
+  auido.play();
+  if (localStorage.clickcount) {
+    localStorage.clickcount = Number(localStorage.clickcount) + 1;
+  } else {
+    localStorage.clickcount = 0;
+  }
+  counter.innerHTML = `Pakde Owi Sudah Kaget ${localStorage.clickcount} kali`;
+  setTimeout(function () {
+    img.src = "./assets/images/pakde_normal.png";
+  }, 500);
+});
